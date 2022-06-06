7e614bf52d924dee2028422004c14aabeb3c4a86
kajela <titoyut56@gmail.com>
2022-06-06 15:57:14 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myObj = new Scanner(System.in);
+        String Instagram;
+        int Password;
+        System.out.println("");
+        System.out.println("***************************");
+        System.out.println("Enter your Instagram credentials");
+        Instagram = myObj.nextLine();
+        System.out.println("");
+        System.out.println("***********************");
+        System.out.println("Enter your password");
+        Password = myObj.nextInt();
+        System.out.println("Select Page: " +Instagram);
+        System.out.println("Enter password: " +Password);
     }
 }
